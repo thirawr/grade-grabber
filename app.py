90f8e22c3236fdf2d128120d3e830237bc91b269
thirawr <tallyndavis@comcast.net>
2016-03-14 19:20:45 -0500

    Added support for flash messages, implemented insertion (fully?), update confirmation query/route added, fuller query page. Simplified result display.

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, url_for, abort, flash
 from flaskext.mysql import MySQL
 
 from cred import Cred
 app = Flask(__name__)
+app.secret_key = 'i_really_love_cookies'
 mysql = MySQL()
 
 app.config['MYSQL_DATABASE_USER'] = Cred['MYSQL_DATABASE_USER']
@@ -13,82 +14,125 @@ app.config['MYSQL_DATABASE_HOST'] = Cred['MYSQL_DATABASE_HOST']
 mysql.init_app(app)
 dbConnection = mysql.connect()
 
+# class GradeReport:
+# 	"""
+# 	Class to represent one course to preserve insertion integrity
+# 	Also eases my tired fingers
+# 	"""
+# 	def __init__(self, subject, course_number, title, crn, term, instructor, average_gpa, a_plus, a, a_minus, b_plus, b, b_minus, c_plus, c, c_minus, d_plus, d, d_minus, f):
+# 		self.subject = subject
+# 		self.course_number = course_number
+# 		self.title = title
+# 		self.crn = crn 
+# 		self.term = term
+# 		self.instructor = instructor
+# 		self.average_gpa = average_gpa
+# 		self.a_plus = a_plus
+# 		self.a = a
+# 		self.a_minus = a_minus
+# 		self.b_plus = b_plus
+# 		self.b = b
+# 		self.b_minus = b_minus
+# 		self.c_plus = c_plus
+# 		self.c = c
+# 		self.c_minus = c_minus
+# 		self.d_plus = d_plus
+# 		self.d = d
+# 		self.d_minus = d_minus
+# 		self.f = f
+
 def query_all():
 	with dbConnection as cursor:
-		cursor.execute('SELECT subject, course_number, title, average_gpa, crn, term FROM course')
+		cursor.execute('SELECT * FROM course')
 		results = cursor.fetchall()
-	output = []
-	for row in results:
-		rowMap = {}
-		for attribute in row:
-			if row.index(attribute) == 0:
-				rowMap['subject'] = str(attribute)
-			elif row.index(attribute) == 1:
-				rowMap['course_number'] = int(attribute)
-			elif row.index(attribute) == 2:
-				rowMap['title'] = str(attribute)
-			elif row.index(attribute) == 3:
-				rowMap['average_gpa'] = float(attribute)
-			elif row.index(attribute) == 4:
-				rowMap['crn'] = attribute
-			else:
-				rowMap['term'] = attribute
-		output.append(rowMap)
-	return output
+	return results
 
-#Record is a list of values:
+def query_by_crn_term(crn, term):
+	with dbConnection as cursor:
+		query = 'SELECT * FROM course WHERE crn = (%s) AND term = (%s)'
+		args = crn, term
+		cursor.execute(query, args)
+		return cursor.fetchone(),
+
+#Record is a dictionary of values:
 #[Subject, Course#, Title, CRN, Term, Average GPA, A+, A, A-, B+, B, B-, C+, C, C-, D+, D, D-, F]
 def insert_record(record):
-	subject = record[0]
-	course_number = record[1]
-	title = record[2]
-	CRN = record[3]
-	term = record[4]
-	avg_gpa = record[5]
-	a_plus_count = record[6]
-	a_count = record[7]
-	a_minus_count = record[8]
-	b_plus_count = record[9]
-	b_count = record[10]
-	b_minus_count = record[11]
-	c_plus_count = record[12]
-	c_count = record[13]
-	c_minus_count = record[14]
-	d_plus_count = record[15]
-	d_count = record[16]
-	d_minus_count = record[17]
-	f_count = record[18]
-	instructor_name = record[19]
-	section = record[20]
-
+	#@Todo: implement N/A case for letter grades?
+	#assumes all fields are filled
 	with dbConnection as cursor:
 		query = 'SELECT COUNT(*) FROM course WHERE crn = (%s) AND term = (%s)'
-		args = int(CRN), int(term)
+		args = str(record['crn']), str(record['term'])
 		cursor.execute(query, args)
-		results = cursor.fetchall()
-		print results
-			#error = 'Course with identical CRN, Term already exists in our records.'
-			#return error
-		#else:
-		insertion = 'INSERT INTO course (crn, term, subject, course_number, title, average_gpa, a_plus_count, a_count, a_minus_count, b_plus_count, b_count, b_minus_count, c_plus_count, c_count, c_minus_count, d_plus_count, d_count, d_minus_count, f_count, instructor_name, section) VALUES ((%s), (%s), (%s), (%s), (%s), (%s), (%s), (%s), (%s), (%s), (%s), (%s), (%s), (%s), (%s), (%s), (%s), (%s), (%s), (%s), (%s));'
-		args = CRN, term, subject, course_number, title, avg_gpa, a_plus_count, a_count, a_minus_count, b_plus_count, b_count, b_minus_count, c_plus_count, c_count, c_minus_count, d_plus_count, d_count, d_minus_count, f_count, instructor_name, section 
-		cursor.execute(insertion, args)
-		return 'Success'
-
+		results = cursor.fetchall() 
+		if results[0][0] > 0:
+			flash('Course with identical CRN, term already exists in database - try updating the record instead')
+			return None
+	insertion = 'INSERT INTO course (subject, course_number, title, crn, term, section, instructor_name, average_gpa, a_plus_count, a_count, a_minus_count, b_plus_count, b_count, b_minus_count, c_plus_count, c_count, c_minus_count, d_plus_count, d_count, d_minus_count, f_count) VALUES '
+	values = "('" + record['subject'] + "', '" + record['number'] + "', '" + record['title'] + "', '" + record['crn'] + "', '" + record['term'] + "', '" + record['section'] + "', '" + record['instructor'] + "', " + record['avg_gpa'] + ', ' + record['aplus'] + ', ' + record['a'] + ', ' + record['aminus'] + ', ' + record['bplus'] + ', ' + record['b'] + ', ' + record['bminus'] + ', ' + record['cplus'] + ', ' + record['c'] + ', ' + record['cminus'] + ', ' + record['dplus'] + ', ' + record['d'] + ', ' + record['dminus'] + ', ' + record['f'] + ');'
+	insertion = insertion + values
+	with dbConnection as cursor:
+		cursor.execute(insertion)
+		dbConnection.commit()
+		flash('Insertion successful')
+		return 1
 
 @app.route("/")
 def front():
 	results = query_all()
 	return render_template('query.html', results=results)
 
-@app.route("/modify")
+@app.route("/modify", methods=['GET', 'POST'])
 def modify():
 	#status = insert_record(['CS', '411', 'Database Systems', '11111', '120313', '3.5', 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 'Tyler Davis', 2112])
-	return render_template('modify.html', status=status)
+	if request.method == 'POST':
+		query = 'SELECT * FROM course '
+		
+		#Searching for a course
+		if 'search' in request.form:
+			subject = request.form['subject']
+			number = request.form['number']
+			with dbConnection as cursor:
+				query = query + 'WHERE subject = (%s) AND course_number = (%s);'
+				args = str(subject), str(number)
+				cursor.execute(query, args)
+				results = cursor.fetchall()
+			return render_template('query.html', results=results)
+		
+		#Updating a record
+		elif 'update' in request.form:
+			subject = request.form['subject']
+			number = request.form['number']
+			crn = request.form['crn']
+			#print crn
+			with dbConnection as cursor:
+				query = 'SELECT * FROM course WHERE subject = (%s) AND course_number = (%s) AND crn = (%s);'
+				args = subject, number, crn
+				cursor.execute(query, args)
+				result = cursor.fetchone()
+			#Exclude W, Instructor, Section
+			result = result,
+			print result
+			result = result[1:4]+result[len(result)-1]+result[4:]
+			return render_template('update.html', result=result)
 
-@app.route('/update')
-def update():
-	return render_template('update.html')
+		#Inserting a record
+		elif 'insert' in request.form:
+			#minimal form validation
+			if request.form['subject'] and request.form['number'] and request.form['title'] and request.form['crn'] and request.form['term'] and request.form['section']:
+				status = insert_record(request.form)
+				if status == None:
+					return render_template('modify.html')
+				else:
+					return render_template('query.html', results=query_by_crn_term(request.form['crn'], request.form['term']))
+			else:
+				flash('Insertion requires the following fields at minimum: subject, course number, title, CRN and section.')
+			return render_template('modify.html')
+
+		#@todo: Deleting a record
+		elif 'delete' in request.form:
+			pass
+	else:
+		return render_template('modify.html')
 
 if __name__ == "__main__":
 	app.run(debug=True)
