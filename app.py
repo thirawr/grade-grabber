df8da4fad005c9f6cecd1d3d2df0f763f250c172
thirawr <tallyndavis@comcast.net>
2016-03-15 22:51:40 -0500

    Cleaner insert function

--- app.py
+++ app.py
@@ -14,34 +14,8 @@ app.config['MYSQL_DATABASE_HOST'] = Cred['MYSQL_DATABASE_HOST']
 mysql.init_app(app)
 dbConnection = mysql.connect()
 
-# class GradeReport:
-# 	"""
-# 	Class to represent one course to preserve insertion integrity
-# 	Also eases my tired fingers
-# 	"""
-# 	def __init__(self, subject, course_number, title, crn, term, instructor, average_gpa, a_plus, a, a_minus, b_plus, b, b_minus, c_plus, c, c_minus, d_plus, d, d_minus, f):
-# 		self.subject = subject
-# 		self.course_number = course_number
-# 		self.title = title
-# 		self.crn = crn 
-# 		self.term = term
-# 		self.instructor = instructor
-# 		self.average_gpa = average_gpa
-# 		self.a_plus = a_plus
-# 		self.a = a
-# 		self.a_minus = a_minus
-# 		self.b_plus = b_plus
-# 		self.b = b
-# 		self.b_minus = b_minus
-# 		self.c_plus = c_plus
-# 		self.c = c
-# 		self.c_minus = c_minus
-# 		self.d_plus = d_plus
-# 		self.d = d
-# 		self.d_minus = d_minus
-# 		self.f = f
-
 def query_all():
+	#__entry__ handles connection into cursor
 	with dbConnection as cursor:
 		cursor.execute('SELECT * FROM course')
 		results = cursor.fetchall()
@@ -57,29 +31,30 @@ def query_by_crn_term(crn, term):
 #Record is a dictionary of values:
 #[Subject, Course#, Title, CRN, Term, Average GPA, A+, A, A-, B+, B, B-, C+, C, C-, D+, D, D-, F]
 def insert_record(record):
-	#@Todo: implement N/A case for letter grades?
-	#assumes all fields are filled
 	with dbConnection as cursor:
 		query = 'SELECT COUNT(*) FROM course WHERE crn = (%s) AND term = (%s)'
-		args = str(record['crn']), str(record['term'])
+		args = record['crn'], record['term']
 		cursor.execute(query, args)
 		results = cursor.fetchall() 
 		if results[0][0] > 0:
-			flash('Course with identical CRN, term already exists in database - try updating the record instead')
+			flash('Course with identical CRN, term already exists in database - try updating the record instead.')
 			return None
-	insertion = 'INSERT INTO course (subject, course_number, title, crn, term, section, instructor_name, average_gpa, a_plus_count, a_count, a_minus_count, b_plus_count, b_count, b_minus_count, c_plus_count, c_count, c_minus_count, d_plus_count, d_count, d_minus_count, f_count) VALUES '
-	values = "('" + record['subject'] + "', '" + record['number'] + "', '" + record['title'] + "', '" + record['crn'] + "', '" + record['term'] + "', '" + record['section'] + "', '" + record['instructor'] + "', " + record['avg_gpa'] + ', ' + record['aplus'] + ', ' + record['a'] + ', ' + record['aminus'] + ', ' + record['bplus'] + ', ' + record['b'] + ', ' + record['bminus'] + ', ' + record['cplus'] + ', ' + record['c'] + ', ' + record['cminus'] + ', ' + record['dplus'] + ', ' + record['d'] + ', ' + record['dminus'] + ', ' + record['f'] + ');'
-	insertion = insertion + values
+	insertion = 'INSERT INTO course (subject, course_number, title, crn, term, section, instructor_name, average_gpa, a_plus_count, a_count, a_minus_count, b_plus_count, b_count, b_minus_count, c_plus_count, c_count, c_minus_count, d_plus_count, d_count, d_minus_count, f_count) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)'
+	subject = record['subject'].upper()
+	section = record['section'].upper()
+	if not (record['avg_gpa'] and record['aplus'] and record['a'] and record['aminus'] and record['bplus'] and record['b'] and record['bminus'] and record['cplus'] and record['c'] and record['cminus'] and record['dplus'] and record['d'] and record['dminus'] and record['f']):
+		values = (subject, record['number'], record['title'], record['crn'], record['term'], section, record['instructor'], None, None, None, None, None, None, None, None, None, None, None, None, None, None)
+	else:
+		values = (subject, record['number'], record['title'], record['crn'], record['term'], section, record['instructor'], record['avg_gpa'], record['aplus'], record['a'], record['aminus'], record['bplus'], record['b'], record['bminus'], record['cplus'], record['c'], record['cminus'], record['dplus'], record['d'], record['dminus'], record['f'])
 	with dbConnection as cursor:
-		cursor.execute(insertion)
+		cursor.execute(insertion, values)
 		dbConnection.commit()
 		flash('Insertion successful')
 		return 1
 
 @app.route("/")
 def front():
-	results = query_all()
-	return render_template('query.html', results=results)
+	return render_template('query.html', results=query_all())
 
 @app.route("/modify", methods=['GET', 'POST'])
 def modify():
@@ -109,16 +84,16 @@ def modify():
 				args = subject, number, crn
 				cursor.execute(query, args)
 				result = cursor.fetchone()
-			#Exclude W, Instructor, Section
+			#Reorder columns 
 			result = result,
 			print result
-			result = result[1:4]+result[len(result)-1]+result[4:]
+			result = result[0]
 			return render_template('update.html', result=result)
 
 		#Inserting a record
 		elif 'insert' in request.form:
 			#minimal form validation
-			if request.form['subject'] and request.form['number'] and request.form['title'] and request.form['crn'] and request.form['term'] and request.form['section']:
+			if request.form['subject'] and request.form['number'] and request.form['title'] and request.form['crn'] and request.form['term'] and request.form['section'] and request.form['instructor']:
 				status = insert_record(request.form)
 				if status == None:
 					return render_template('modify.html')
