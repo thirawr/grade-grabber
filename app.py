2526e703ace13c31c45996bcbb501b809fc6d90c
thirawr <tallyndavis@comcast.net>
2016-04-19 13:39:56 -0500

    Added about, 404 templates. Tweaked query page.

--- app.py
+++ app.py
@@ -455,6 +455,13 @@ def modify():
 	else:
 		return render_template('modify.html')
 
+@app.route("/about")
+def about():
+	return render_template("about.html")
+
+@app.errorhandler(404)
+def page_not_found(e):
+	return render_template('404.html'), 404
 
 if __name__ == "__main__":
 	app.run(debug=True)
